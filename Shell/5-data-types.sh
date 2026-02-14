572cca55af12f42438794e762c2dd9fa41f82c2f
param <param@gmail.com>
2026-02-14 17:55:11 +0530

    shell

--- Shell/5-data-types.sh
+++ Shell/5-data-types.sh
@@ -13,4 +13,5 @@ FRUITS=("Apple" "Banana" "Pomo")
 echo "Fruits are: ${FRUITS[@]}"
 echo "First Fruit is: ${FRUITS[0]}"
 echo "Second Fruit is: ${FRUITS[1]}"
-echo "Third Fruit is: ${FRUITS[2]}"})
+echo "Third Fruit is: ${FRUITS[2]}"
+echo "Third Fruit is: ${FRUITS[*]}"
