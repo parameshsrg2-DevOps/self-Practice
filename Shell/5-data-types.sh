b6552c8680afd2febf6b066a68246de0fcba5b9b
param <param@gmail.com>
2026-02-14 17:53:35 +0530

    shell

--- Shell/5-data-types.sh
+++ Shell/5-data-types.sh
@@ -8,9 +8,9 @@ SUM=$(($NUM1+$NUM2))
 echo "Sum is: $SUM"
 
 # Array
-#FRUITS=("Apple" "Banana" "Pomo")
+FRUITS=("Apple" "Banana" "Pomo")
 
-#echo "Fruits are: ${FRUITS[@]}"
-#echo "First Fruit is: ${FRUITS[0]}"
-#echo "Second Fruit is: ${FRUITS[1]}"
-#echo "Third Fruit is: ${FRUITS[2]}"})
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"})
