752f1657e6f4c164199e92a4d83de5bb7efd6135
param <param@gmail.com>
2026-02-14 16:48:39 +0530

    shell

--- Shell/1-variables.sh
+++ Shell/1-variables.sh
@@ -14,4 +14,4 @@ echo "$PERSON2:: I am learning Shell. What about you?"
 echo "$P1"::Hi $P2, how did you?
 echo "$P2"::Hi $P1, I am doing good, How is going you $Class?
 echo "$P1"::Going good my $Class, Is any specific any chalanges find in your running $Class
-echo "$P2"::As of now, no need to check in further $Class
+echo "$P2"::As of now, No need to check in further $Class
