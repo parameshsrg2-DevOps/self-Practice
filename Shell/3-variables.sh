78b6342ce578a4ce5653ec328da4724c4d2c50ef
param <param@gmail.com>
2026-02-14 17:41:14 +0530

    shell

--- Shell/3-variables.sh
+++ Shell/3-variables.sh
@@ -13,4 +13,4 @@ echo "Script executed in: $END_TIME seconds"
 # Calculate duration
 TOTAL_TIME=$(($END_TIME - $START_TIME))
 
-echo "Script executed in: $TOTAL_TIME seconds"
+echo "Script executed in: $TOTAL_TIME"
