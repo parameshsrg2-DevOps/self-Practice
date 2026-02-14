44992a56bae58127cf2907bba3ea08c46ccfecc5
param <param@gmail.com>
2026-02-14 16:57:57 +0530

    shell

--- Shell/2-variables.sh
+++ Shell/2-variables.sh
@@ -2,9 +2,11 @@
 
 echo "Please Enter your username::"
 read USER_NAME 
-read PASSWORD
 
 echo "User name is $USER_NAME"
-echo "Password is $PASSWORD"
 
 
+echo "Please enter your password::"
+read -s SPASSWORD
+
+echo "Password is $SPASSWORD"
