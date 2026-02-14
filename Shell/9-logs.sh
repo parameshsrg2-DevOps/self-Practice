fdc5194b6a7e5d9e1430b49c896456a7ff00bb5d
param <param@gmail.com>
2026-02-14 18:23:25 +0530

    shell

--- Shell/9-logs.sh
+++ Shell/9-logs.sh
@@ -3,3 +3,8 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
