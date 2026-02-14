d44bfe61bed0bafdbd30a450654bd999ed95666e
param <param@gmail.com>
2026-02-14 16:47:41 +0530

    shell

---
+++ Shell/1-variables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PERSON1=Param # no space between value and =
+PERSON2=Sridhar
+P1=Kavya
+P2=Pooja
+Class=Joindevops
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell. What about you?"
+
+echo "$P1"::Hi $P2, how did you?
+echo "$P2"::Hi $P1, I am doing good, How is going you $Class?
+echo "$P1"::Going good my $Class, Is any specific any chalanges find in your running $Class
+echo "$P2"::As of now, no need to check in further $Class
